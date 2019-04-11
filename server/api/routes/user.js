868006c6e1ca9e56e56f372bfecb659f8cab3b16
sequeira_ishita_001403357 <sequeira.i@husky.neu.edu>
2019-04-11 02:53:11 -0400

    updated the comments as asked  in PR1

--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -1,10 +1,14 @@
 module.exports = function (app) {
   const UserController = require('../controllers/user-controller');
-  // Sticky Routes for search and create.
+  /**
+   * @desc User Routes for search and create.
+    */
   app.route('/users').get(UserController.getAll)
     .post(UserController.post);
 
-  // Sticky Routes for get, update and delete.
+  /**
+   * @desc User Routes for get, update and delete.
+   */
   app.route('/users/:userId').get(UserController.getOne)
     .put(UserController.put)
     .delete(UserController.delete);
