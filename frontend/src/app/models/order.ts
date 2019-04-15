3e066946e7e3cb60619de8de5f3055a87dc98605
Shashank Sharma <sharma.shas@husky.neu.edu>
2019-04-15 19:59:13 -0400

    Added model and service to fetch orders for the trade-component

---
+++ frontend/src/app/models/order.ts
@@ -0,0 +1,18 @@
+export class Order {
+
+  constructor(
+    public _id: String,
+    public user_id: String,
+    public status: String,
+    public matched_order_id: String,
+    public buy_or_sell: String,
+    public from_coin: String,
+    public from_qty: Number,
+    public from_value: Number,
+    public to_coin: String,
+    public to_qty: Number,
+    public to_value: Number,
+    public created_date: Date,
+    public completion_date: Date
+  ) { }
+}
