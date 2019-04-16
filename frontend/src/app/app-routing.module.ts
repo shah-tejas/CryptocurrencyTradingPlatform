88592c1056879ad9f6d2f78261d172960ec63905
Tejas Shah <shah.te@husky.neu.edu>
2019-04-15 22:15:26 -0400

    logic for withdraw wallet component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WalletComponent } from './wallet/wallet.component';
 import { LoadWalletComponent } from './wallet/load-wallet/load-wallet.component';
+import { WithdrawWalletComponent } from './wallet/withdraw-wallet/withdraw-wallet.component';
 
 const routes: Routes = [
   {path: 'wallet', component: WalletComponent},
-  {path: 'loadWallet', component: LoadWalletComponent}
+  {path: 'loadWallet', component: LoadWalletComponent},
+  {path: 'withdrawWallet', component: WithdrawWalletComponent}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,26 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { WalletComponent } from './wallet/wallet.component';
 import { LoadWalletComponent } from './wallet/load-wallet/load-wallet.component';
+import { WithdrawWalletComponent } from './wallet/withdraw-wallet/withdraw-wallet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     WalletComponent,
-    LoadWalletComponent
+    LoadWalletComponent,
+    WithdrawWalletComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
