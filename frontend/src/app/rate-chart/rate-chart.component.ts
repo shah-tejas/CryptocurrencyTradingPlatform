6cef680d49944944d266ca0b9c300c19454afd81
cyrilsebastian1811 <sebastian.c@husky.neu.edu>
2019-04-19 10:20:39 -0400

    Charts functionality added

--- frontend/src/app/rate-chart/rate-chart.component.ts
+++ frontend/src/app/rate-chart/rate-chart.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck } from '@angular/core';
 import { RateListService } from '../services/rate-list.service';
 import { Rate } from '../models/rate';
 import { Router } from '@angular/router';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-rate-chart',
@@ -9,168 +10,99 @@ import { Router } from '@angular/router';
   styleUrls: ['./rate-chart.component.scss']
 })
 export class RateChartComponent implements OnInit {
-    private BTC: any[];
-    private ETH: any[];
-    private LTC: any[];
-    private EOS: any[];
-    private display: boolean = true;
+  private chart = [];
+  private days: any[];
+  // private BTC: any[];
+  private ETH: any[];
+  private LTC: any[];
+  private EOS: any[];
+  private display: boolean = true;
 
-  private BTCobserver: any = {
-    next: data => {
-      this.BTC = data;
-      console.log(this.BTC);
-    },
-    error: err => console.log(err)
-  };
+  // private BTCobserver: any = {
+  //   next: data => {
+  //     this.BTC = data.map(res => res.usdvalue);
+  //   }, error: err => console.log(err)
+  // };
 
   private ETHobserver: any = {
     next: data => {
-      this.ETH = data;
-      console.log(this.ETH);
-    },
-    error: err => console.log(err)
+      this.ETH = data.map(res => res.usdvalue);
+    }, error: err => console.log(err)
   };
 
   private LTCobserver: any = {
     next: data => {
-      this.LTC = data;
-      console.log(this.LTC);
-    },
-    error: err => console.log(err)
+      this.LTC = data.map(res => res.usdvalue);
+    }, error: err => console.log(err)
   };
 
   private EOSobserver: any = {
     next: data => {
-      // this.EOS = data;
-      this.iterate(data,this.EOS);
-      console.log("-------------");
-      this.display = true;
-      console.log(this.display);
+      this.EOS = data.map(res => res.usdvalue);
+      this.days = data.map(res => res.insert_date.toString().split('T')[0]);
+      console.log(this.EOS);
+      console.log(this.days);
+      this.helper();
     },
     error: err => console.log(err)
   };
 
-  iterate = function(data: Rate[], container: any[]) {
-    this.container = new Array();
-    for (var temp of data) {
-        this.container.push({usdvalue: temp.usdvalue});
-        console.log(temp.usdvalue);
-    }
-
-    console.log(this.container);
-    console.log(this.Keith);
-  }
-  //
   constructor(private rateService: RateListService, private router: Router) {
-
   }
-  //
-  ngOnInit() {
+
+  ngOnInit(){
     if (!localStorage.getItem('token')) {
       this.router.navigateByUrl('/login');
     }else{
-      this.rateService.get("BTC").subscribe(this.BTCobserver);
+      // this.rateService.get("BTC").subscribe(this.BTCobserver);
       this.rateService.get("ETH").subscribe(this.ETHobserver);
       this.rateService.get("LTC").subscribe(this.LTCobserver);
-      // this.rateService.get("EOS").subscribe(this.EOSobserver);
+      this.rateService.get("EOS").subscribe(this.EOSobserver);
+
     }
   }
 
-  days: any[] = [
-          { Day: '2019-04-18' },
-          { Day: '2019-04-19' },
-          { Day: '2019-05-20' },
-          { Day: '2019-06-21' },
-          { Day: '2019-07-22' },
-          { Day: '2019-08-23' },
-          { Day: '2019-08-24' }
-   ];
-   Keith: any[] = [
-       { Minutes: 30 },
-       { Minutes: 50 },
-       { Minutes: 30 },
-       { Minutes: 35 },
-       { Minutes: 20 },
-       { Minutes: 30 },
-       { Minutes: 60 }
-   ];
-   Erica: any[] = [
-       { Minutes: 15 },
-       { Minutes: 25 },
-       { Minutes: 20 },
-       { Minutes: 25 },
-       { Minutes: 20 },
-       { Minutes: 20 },
-       { Minutes: 45 }
-   ];
-   George: any[] = [
-       { Minutes: 25 },
-       { Minutes: 30 },
-       { Minutes: 25 },
-       { Minutes: 45 },
-       { Minutes: 25 },
-       { Minutes: 30 },
-       { Minutes: 90 }
-   ];
-   // EOS: any[] = [
-   //     { usdvalue: 212 },
-   //     { usdvalue: 100 },
-   //     { usdvalue: 149 },
-   //     { usdvalue: 190 },
-   //     { usdvalue: 167 },
-   //     { usdvalue: 132 },
-   //     { usdvalue: 90 }
-   // ];
-   padding: any = { left: 5, top: 5, right: 40, bottom: 5 };
-   titlePadding: any = { left: 90, top: 0, right: 0, bottom: 10 };
- getWidth() : any {
-   if (document.body.offsetWidth < 850) {
-     return '90%';
-   }
-
-   return 850;
- }
-
-   xAxis: any =
-   {
-       dataField: 'Day',
-       gridLines: { visible: true }
-   };
-   valueAxis: any =
-   {
-       visible: true,
-       title: { text: 'USD' }
-   };
-   seriesGroups: any[] =
-   [
-       {
-           type: 'stackedline',
-           source: this.Keith,
-           series: [
-               { dataField: 'Minutes', displayText: 'BTC' }
-           ]
-       },
-       {
-           type: 'stackedline',
-           source: this.Erica,
-           series: [
-               { dataField: 'Minutes', displayText: 'ETH' }
-           ]
-       },
-       {
-           type: 'stackedline',
-           source: this.George,
-           series: [
-               { dataField: 'Minutes', displayText: 'LTC' }
-           ]
-       },
-       {
-           type: 'stackedline',
-           source: this.EOS,
-           series: [
-               { dataField: 'usdvalue', displayText: 'EOS' }
-           ]
-       }
-   ];
-
+  helper = function() {
+    this.chart = new Chart('canvas', {
+      type: 'line',
+      data: {
+        labels: this.days,
+        datasets: [
+          {
+            data: this.BTC,
+            borderColor: "red",
+            fill: false
+          },
+          {
+            data: this.ETH,
+            borderColor: "pink",
+            fill: false
+          },
+          {
+            data: this.LTC,
+            borderColor: "green",
+            fill: false
+          },
+          {
+            data: this.EOS,
+            borderColor: "red",
+            fill: false
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: true
+          }]
+        }
+      }
+    })
+  }
 }
