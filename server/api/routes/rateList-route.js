54a5eb599e8ee34bbb32856a1cac01b500b97729
Shashank Sharma <sharma.shas@husky.neu.edu>
2019-04-23 19:07:20 -0400

    Added comments to the files

--- server/api/routes/rateList-route.js
+++ server/api/routes/rateList-route.js
@@ -1,21 +1,31 @@
-//Setting routes for the API
+/**
+   * @desc Routes for fetching and creating rates for different coins
+    */
 'use strict';
 module.exports = ((app) => {
     const rateController = require('../controllers/rateList-controller');
-    //Routes to add and retrieve coin-rates
+/**
+   * @desc Rate Routes to fetch and create rate for coins
+    */
     app.route('/currentRate')
-        .get(rateController.allCurrentRates) //retrieve all current-rates from DB
-        .post(rateController.addNewCurrentRate);//add new current-rate to DB
+        .get(rateController.allCurrentRates) 
+        .post(rateController.addNewCurrentRate);
 
-    //Route to retrieve a particular coin-rate from DB based on coin-name
+/**
+   * @desc Rate Route to fetch current rate for respective coin
+    */
     app.route('/currentRate/:coinname')
         .get(rateController.getCoinCurrentRate);
 
-     //Routes to fetch rate-history of all coins
+/**
+   * @desc Rate Route to fetch all rates for all coins
+    */
      app.route('/rateHistory')
      .get(rateController.allRateHistory);
 
-    //Route to fetch rate-history of a particular coin based on coin-name
+/**
+   * @desc Rate Route to fetch all rates for specific coin
+    */
     app.route('/rateHistory/:coinname')
         .get(rateController.allRateHistoryCoin);
  
--- server/api/services/rateList-service.js
+++ server/api/services/rateList-service.js
@@ -1,40 +1,60 @@
-//Setting up Service for API to interact with DB
+/**
+* @desc Services for fetching and adding rates for coins
+*/
 'use strict';
+/**
+* @desc variable for mongoose library
+*/
 const mongoose = require('mongoose'),
+/**
+* @desc variable for Rate-List model
+*/
 RateList = mongoose.model('RateList');
 
-//Service to retrieve current-rates of all coins from DB
+/**
+   * @desc Service to retrieve current-rates of all coins from DB
+    */
 exports.getAllCurrentRates = (() => {
     const promise = RateList.find({'active': "A"}).exec();
     return promise;
 });
 
-//Service to mark all previous-rates as obsolete to DB
+/**
+   * @desc Service to mark all previous-rates as obsolete to DB
+    */
 exports.voidPreviousRates = ((rate) => {
     const promise = RateList.updateMany({'coinname': rate.coinname}, { 'active': 'I'}).exec();
     return promise;
 });
 
-//Service to add current-rate of a coin to DB
+/**
+   * @desc Service to add current-rate of a coin to DB
+    */
 exports.save = ((rate) => {
     const newCurrentRate = new RateList(rate);
     const promise = newCurrentRate.save();
     return promise;
 });
 
-//Service to fetch current-rate of single coin from DB using coin-name
+/**
+   * @desc Service to fetch current-rate of single coin from DB using coin-name
+    */
 exports.getCoinCurrentRate = ((coinname) => {
     const promise = RateList.find({coinname: coinname , active: "A" }).exec();
     return promise
 });
 
-//Service to retrieve all-rates of all coins from DB
+/**
+   * @desc Service to retrieve all-rates of all coins from DB
+    */
 exports.getAllRates = (() => {
     const promise = RateList.find().exec();
     return promise;
 });
 
-//Service to fetch all-rates of single coin from DB using coin-name
+/**
+   * @desc Service to fetch all-rates of single coin from DB using coin-name
+    */
 exports.getAllRatesCoin = ((coinname) => {
     const promise = RateList.find({coinname: coinname }).exec();
     return promise
